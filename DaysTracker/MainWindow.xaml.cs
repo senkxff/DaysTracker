64bfede42764aa3be6b9bcc37d644afd4f88c5de
senkxff <xxzarenf@gmail.com>
2025-10-02 10:29:34 +0300

    Create main elements

--- DaysTracker/MainWindow.xaml.cs
+++ DaysTracker/MainWindow.xaml.cs
@@ -19,6 +19,32 @@ namespace DaysTracker
         public MainWindow()
         {
             InitializeComponent();
+            this.MouseLeftButtonDown += (s, e) => DragMove();
+        }
+
+        private void Close_btn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();    
+        }
+
+        private void Add_btn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Add_btn.Foreground = Brushes.Green;
+        }
+
+        private void Add_btn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Add_btn.Foreground = Brushes.White;
+        }
+
+        private void Minus_btn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Minus_btn.Foreground = Brushes.Red;
+        }
+
+        private void Minus_btn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Minus_btn.Foreground = Brushes.White;
         }
     }
 }
